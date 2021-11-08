418db6b76e75beb96688b126502b09fec78c560c
Adil <ascodesofficial@gmail.com>
2021-11-08 22:26:53 +0500

    Dashboard file Ui completed

--- dashboard.php
+++ dashboard.php
@@ -4,9 +4,45 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hi!</title>
+    <title>Welcome!</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="dashboard_style.css">
 </head>
 <body>
-    <h1>Hello World!</h1>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12 header">
+                <div class="nav">
+                    <button class="btn">Log Out</button>
+                    <button class="btn ml-2">Edit Profile</button>
+                </div>
+                <div class="add-items">
+                    <input type="text" class="form-control col-lg-4" placeholder="Add To-Do item">
+                    <input type="submit" name="add" value="Add" class="btn btn-primary ml-2">
+                </div>
+            </div>
+            <div class="col-lg-12">
+                <table class="table">
+                    <thead>
+                        <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">ToDo's</th>
+                        <th scope="col">Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <th scope="row">1</th>
+                        <td>Wake-up at 10:00am</td>
+                        <td><button class="btn btn-danger" name="delete">Delete</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
